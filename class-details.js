c050a45d5230e1d6930d189f35a1ba7bfce93c2d
hellomafiya99-tech <hellomafiya99@gmail.com>
2026-04-02 12:04:00 +0530

    Update class-details.js

--- class-details.js
+++ class-details.js
@@ -6,15 +6,18 @@ document.getElementById("title").innerText=cls.name;
 
 let html="";
 
+// Teachers
 cls.teachers.forEach(t=>{
   html+=`<div class="card">${t.name}<br>${t.subject}<br>${t.period}</div>`;
 });
 
+// Marks
 let marks=JSON.parse(localStorage.getItem("marks"))||[];
 marks.forEach(m=>{
   html+=`<div class="card">📊 ${m.name}: ${m.marks}</div>`;
 });
 
+// Attendance
 let att=JSON.parse(localStorage.getItem("attendance"))||[];
 att.forEach(a=>{
   html+=`<div class="card">📅 ${a.name}: ${a.days}</div>`;
